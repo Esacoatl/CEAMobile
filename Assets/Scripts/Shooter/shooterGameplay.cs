e466def6715ec6d9f9e0107d87a5ce0c3630b5f5
esaucedillo <jorge.cedillo@bsdenterprise.com>
2020-09-07 14:31:02 -0500

    avance de jugabilidad shooter

---
+++ Assets/Scripts/Shooter/shooterGameplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooterGameplay : MonoBehaviour
+{
+    public int enemysCount = 3;
+
+    public GameObject[] objectsLife = new GameObject[4];
+    public GameObject[] objectsEnemys = new GameObject[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
