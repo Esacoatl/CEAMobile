67f349174827759cc285bf4e3ee2cc730bf7524d
esaucedillo <jorge.cedillo@bsdenterprise.com>
2020-09-25 22:57:53 -0500

    Correcciones Hueya Hidrica en niveles, particulas en Buscador

---
+++ Assets/Scripts/GameFunctionals/hueyaHidrInGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class hueyaHidrInGame : MonoBehaviour
+{
+    public Text litrosScoreTextInGame;
+    // score general de litros ahorrados
+    public static int litrosScoreInGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        litrosScoreInGame = PlayerPrefs.GetInt("litrosSum");
+        string litrosText = litrosScoreInGame.ToString();
+        litrosScoreTextInGame.text = "Hueya Hidrica: " + litrosText + " Litros";
+    }
+}
