7ff700b3e876eb039278201c7fe5f1bdc3a06675
esaucedillo <jorge.cedillo@bsdenterprise.com>
2020-10-06 18:40:37 -0500

    consumo de litros y prefab creados en mainGameplay

---
+++ Assets/Scripts/MainGameplay/mainGameplayController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class mainGameplayController : MonoBehaviour
+{
+    public Text litrosScoreText;
+    public static int litrosScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        litrosScore = PlayerPrefs.GetInt("litrosSum");
+        string litrosText = litrosScore.ToString();
+        litrosScoreText.text = "Huella Hidrica: " + litrosText + " Litros";
+    }
+
+}
