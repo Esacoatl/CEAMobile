a7d9a735401057cb73f6e5ea0f123972e3964bca
esaucedillo <jorge.cedillo@bsdenterprise.com>
2020-10-06 17:37:10 -0500

    Base de Main Gameplay terminada

---
+++ Assets/Scripts/MainGameplay/triggerMessExtoraz.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerMessExtoraz : MonoBehaviour
+{
+    public string animationIn = "InExtorazMess";
+    public string animationOut = "OutExtorazMess";
+    public GameObject mensajeCanvas;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log(obj.tag);
+        if (obj.tag == "PlayerMessArea")
+        {
+            mensajeCanvas.SetActive(true);
+            DOTween.Restart(animationIn);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PlayerMessArea")
+        {
+            DOTween.Restart(animationOut);
+        }
+    }
+}
