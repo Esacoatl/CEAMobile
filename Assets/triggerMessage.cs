9eab5458600e47df52963248586caaafdf059e5d
esaucedillo <jorge.cedillo@bsdenterprise.com>
2020-10-06 16:10:04 -0500

    Chuveje con inicio de gamplay en nivel

---
+++ Assets/triggerMessage.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerMessage : MonoBehaviour
+{
+    public string animationIn = "InChuveje";
+    public string animationOut = "OutChuveje";
+    public GameObject mensajeCanvas;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log(obj.tag);
+        if (obj.tag == "PlayerMessArea")
+        {
+            mensajeCanvas.SetActive(true);
+            DOTween.Restart(animationIn);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PlayerMessArea")
+        {
+            DOTween.Restart(animationOut);
+        }
+    }
+}
